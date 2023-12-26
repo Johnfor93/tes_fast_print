8195e7a72d16ffa830156735967fc34af7a679a9
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-12-27 06:06:43 +0700

    Finalize

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Cara Menjalankan Aplikasi
+
+1. Copy .env.example menjadi .env lalu ubah variabel .env sesuai environment
+
+2. File database.sql merupakan file struktur database dengan menggunakan postgresql. Jalankan code di file ini.
+
+3. Jalankan script yang ada di flaskrun.sh ke dalam terminal berbasis linux. Untuk windows dapat dijalankan melalui git bash.
--- app.py
+++ app.py
@@ -57,7 +57,10 @@ def getOneProduk(id):
     if(data["success"] == True):
         dataProduk = data["data"][0]
     else:
-        return make_response(data, 400)
+        return make_response(jsonify({
+            "success": False,
+            "message": "Data tidak dapat ditemukan"
+        }), 400)
     
     return make_response(jsonify({
         "success": True,
@@ -68,15 +71,46 @@ def editProduk(id):
     content = request.form
 
     # Validate each input
+    error = ""
+    if(not('nama' in content.keys()) or len(content['nama']) == 0):
+        error+="Nama Produk Tidak Boleh Kosong! "
+    if(not('harga' in content.keys()) or len(content['harga']) == 0):
+        error+="Harga Produk Tidak Boleh Kosong! "
+    if(not('Kategori' in content.keys()) or len(content['Kategori']) == 0):
+        error+="Kategori Produk Tidak Boleh Kosong! "
+    if(not('Status' in content.keys()) or len(content['Status']) == 0):
+        error+="Status Produk Tidak Boleh Kosong! "
+
+    if(len(error) != 0):
+        return make_response(jsonify({
+            "success": False,
+            "message": error
+        }), 400)
 
     # Save on database -> call model editProduk
     databaseResponse = produk.editProduk(id, content)
+    
+    # Catch error
+    if(databaseResponse["success"] == False):
+        return make_response(jsonify({
+            "success": False,
+            "message": "Operasi update data gagal dilakukan"
+        }), 500)
+    
     return make_response(databaseResponse, 200)
 
 @app.route("/delete/produk/<id>", methods=["DELETE"])
 def deleteProduk(id):
     # Save on database -> call model deleteProduk
     databaseResponse = produk.deleteProduk(id)
+
+    # Catch error
+    if(databaseResponse["success"] == False):
+        return make_response(jsonify({
+            "success": False,
+            "message": "Operasi hapus data gagal dilakukan"
+        }), 500)
+    
     return make_response(databaseResponse, 200)
 
 @app.route("/create/produk", methods=["POST"])
@@ -84,7 +118,30 @@ def insertProduk():
     content = request.form
 
     # Validate each input
+    error = ""
+    if(not('nama' in content.keys()) or len(content['nama']) == 0):
+        error+="Nama Produk Tidak Boleh Kosong! "
+    if(not('harga' in content.keys()) or len(content['harga']) == 0):
+        error+="Harga Produk Tidak Boleh Kosong! "
+    if(not('Kategori' in content.keys()) or len(content['Kategori']) == 0):
+        error+="Kategori Produk Tidak Boleh Kosong! "
+    if(not('Status' in content.keys()) or len(content['Status']) == 0):
+        error+="Status Produk Tidak Boleh Kosong! "
+
+    if(len(error) != 0):
+        return make_response(jsonify({
+            "success": False,
+            "message": error
+        }), 400)
 
     # Save on database -> call model insertProduk
     databaseResponse = produk.insertProduk(content)
+
+    # Catch error
+    if(databaseResponse["success"] == False):
+        return make_response(jsonify({
+            "success": False,
+            "message": "Operasi penambahan gagal dilakukan"
+        }), 500)
+    
     return make_response(databaseResponse, 200)
